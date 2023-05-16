870b217c05ca9c50cd15d5ddeb59e35d636ddd7c
Leonardo529 <leonardo05.vecina@gmail.com>
2023-05-16 16:46:12 -0300

    final commit

---
+++ src/app/produtos.ts
@@ -0,0 +1,7 @@
+export interface Produto {
+    id : number,
+    nome : string,
+    marca : string,
+    preco : string,
+    quantidade : number,
+}
---
+++ src/app/produtos/produtos.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { ProdutoService } from '../produto.service';
+import { Produto } from '../produtos';
+
+@Component({
+  selector: 'app-produtos',
+  templateUrl: './produtos.component.html',
+  styleUrls: ['./produtos.component.css']
+})
+export class ProdutosComponent implements OnInit{
+
+
+  produtos: Produto[] = [];
+
+
+  constructor (private produtoService: ProdutoService){}
+
+  ngOnInit(): void {
+     this.loadProduto();
+  }
+
+  loadProduto() {
+      this.produtoService.getProduto().subscribe(
+        {
+          next : data => this.produtos = data,
+        }
+      );
+  }
+
+}
